92c2b5e9f9eb575c57f0b90b849bebf2f50492a1
FlevianK <flevian.kanaiza@andela.com>
2017-09-04 17:17:00 +0300

    Add BDD to install NGINX

--- BDD docker-ansible/setup-mysql/features/mysql.feature
+++ BDD docker-ansible/setup-mysql/features/mysql.feature
@@ -1,7 +1,13 @@
-Feature: Install Mysql
+Feature: Install Mysql and nginx
 
-	Scenario: Setup and Install Mysql
-		Given I have a running ubuntu docker container
-		When I run my config scripts
-		Then It should succeed
-		And mysql should be running and listening on port 3306
+#	Scenario: Setup and Install Mysql
+#		Given I have a running ubuntu docker container
+#		When I run my config scripts
+#		Then It should succeed
+#		And mysql should be running on port 3306
+	
+	Scenario: have nginx up and running
+		Given We have a docker container running
+		When I run the scripts
+		Then nginx should be installed
+		And nginx should be running on port 80
--- BDD docker-ansible/setup-mysql/features/steps/mysqlfeature.py
+++ BDD docker-ansible/setup-mysql/features/steps/mysqlfeature.py
@@ -1,5 +1,7 @@
-from behave import given, when, then, step
+from behave import given, when, then, step, use_step_matcher
 from subprocess import Popen, PIPE
+
+use_step_matcher("re")
 
 @given('I have a running ubuntu docker container')
 def step_impl(context):
@@ -23,10 +25,40 @@ def step_impl(context):
 def step_impl(context):
   assert context.ansible_returncode == 0
                                                              
-@then('mysql should be running and listening on port 3306')
-def step_impl(context):
-  proc = Popen(["docker", "exec", context.container_name, "bash", "-c", "exec 7<>/dev/tcp/127.0.0.1/3306"], stdout=PIPE, stderr=PIPE)
+@then('(mysql|nginx) should be running on port {port:d}')
+def step_impl(context, port):
+  proc = Popen(["docker", "exec", context.container_name, "bash", "-c", "exec 7<>/dev/tcp/127.0.0.1/" + port], stdout=PIPE, stderr=PIPE)
   out, err = proc.communicate()
   print(out)
   print(err)
   assert proc.returncode == 0
+
+@given('We have a docker container running')
+def step_impl(context):
+  context.container_name = "scotty-mysql-devops-image"
+  proc = Popen(["docker", "run", "-i", "-d", "--name", context.container_name, "setup-mysql"], stdout=PIPE, stderr=PIPE)
+  container_id, err = proc.communicate()
+  exitcode = proc.returncode
+  assert exitcode == 0
+
+@when('I run the scripts')
+def step_impl(context):
+  proc = Popen(["docker", "exec", context.container_name, "ansible-playbook", "-i", "/tmp/ansible/inventory.ini", "/tmp/ansible/playbook.yml"], stdout=PIPE, stderr=PIPE)
+
+  out, err = proc.communicate()
+  context.ansible_returncode = proc.returncode
+
+@then('nginx should be installed')
+def step_impl(context):
+  assert context.ansible_returncode == 0
+
+@then('nginx should be running on port 80')
+def step_impl(context):
+  proc = Popen(["docker", "exec", context.container_name, "bash", "-c", "exec 7<>/dev/tcp/127.0.0.1/80"], stdout=PIPE, stderr=PIPE)
+
+  out, err = proc.communicate()
+  exitcode = proc.returncode
+  print(out)
+  print(err)
+  assert exitcode == 0
+
